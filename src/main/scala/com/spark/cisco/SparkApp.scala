d858c636371ac2a58c2b19b04a10341f2d1fcd03
Meetu Maltiar <meetu.maltiar@gmail.com>
2015-10-14 13:28:52 +0530

    Added hdfs example

--- src/main/scala/com/spark/cisco/SparkApp.scala
+++ src/main/scala/com/spark/cisco/SparkApp.scala
@@ -6,7 +6,7 @@ import org.apache.spark.SparkContext
 object SparkApp extends App {
   val conf = new SparkConf().setMaster("local").setAppName("My App")
   val sc = new SparkContext(conf)
-  val lines = sc.textFile("src/main/resources/data.txt")
+  val lines = sc.textFile("hdfs://localhost:9000/data.txt")
   val wordCount = lines.map(_.toLowerCase).flatMap(_.split(" ")).groupBy(word => word).map { case (word, wordList) => (word, wordList.size) }
   wordCount foreach { case (word, count) => println(s" $word -> $count") }
   sc.stop()
